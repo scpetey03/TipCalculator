d035131dc50c334c1e5a65d1f39f527f354a2453
scpetey03 <ryanpedersen3@gmail.com>
2014-02-19 22:20:43 -0800

    Initial Commit

---
+++ TipCalculator/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TipCalculator
+//
+//  Created by Ryan Pedersen on 2/19/14.
+//  Copyright (c) 2014 Bootcamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TipCalculator/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  TipCalculator
+//
+//  Created by Ryan Pedersen on 2/19/14.
+//  Copyright (c) 2014 Bootcamp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
